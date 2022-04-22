16beb3158d31ea24000cc1f77172bb211ec2e080
Nkenta123 <nkenta123icloud.com>
2022-04-22 17:40:56 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,13 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)
+    {
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);  
+    }
+        public function show(Post $post)
 {
-    return view('index')->with(['posts' => $post->get()]);
+    return view('show')->with(['post' => $post]);
 }
+
 }
+?>
